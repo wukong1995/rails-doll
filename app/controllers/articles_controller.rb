b07a3931aaeaf7338003501787f8d3c37fddb2a1
wangpeipei <wangpp_1995@163.com>
2017-09-15 14:33:22 +0800

    add action in articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,12 +2,52 @@ class ArticlesController < ApplicationController
   protect_from_forgery with: :null_session
 
   def index
+    @articles = Article.all
   end
 
   def new
+     @article = Article.new
   end
 
   def create
-    render plain: params[:articles].inspect
+    # render plain: params[:articles].inspect
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+    render 'new'
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
