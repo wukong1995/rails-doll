bd3efb89ccbfbe11336ba1a3c37099a9b58fd156
wangpeipei <wangpp_1995@163.com>
2017-09-18 14:20:43 +0800

    add uuid

--- db/migrate/20170918055645_create_comments.rb
+++ db/migrate/20170918055645_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
-    create_table :comments do |t|
+   create_table :comments, id: :uuid, default: 'uuid_generate_v4()' do |t|
       t.string :commenter
       t.text :body
-      t.references :article, foreign_key: true
+      t.references :article, foreign_key: true, type: :uuid
 
       t.timestamps
     end
