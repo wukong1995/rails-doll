31575b0651df36b8c11b21321985140467589580
wangpp <wangpp1995@gmail.com>
2017-09-20 18:17:23 +0800

    adjust application

--- app/frontend/application/index.js
+++ app/frontend/application/index.js
@@ -2,13 +2,11 @@
 
 import '../style/app.scss';
 import deleteArticle from '../page/article';
-import signin from '../page/session/signin';
-import signup from '../page/session/signup';
+import particles from '../page/session';
 
 import { runPage } from '../util/tool';
 
 export const application = () => {
   runPage('articles-index', deleteArticle);
-  runPage('sessions-index', signin);
-  runPage('sessions-new', signup);
+  runPage(['sessions-index','sessions-new'], particles);
 };
