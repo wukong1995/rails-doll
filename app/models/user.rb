528d8ed5b484be519b8218e6f9bf16aeee1c60bd
wangpp <wangpp1995@gmail.com>
2017-09-22 13:08:11 +0800

    add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true
-  validates :user_name, presence: true, length: { minimum: 4, maxnum: 16}
-  validates :password, presence: true, length: { minimum: 6}
+  validates :user_name, presence: true, length: { minimum: 4, maxnum: 20}
 end
 
 # id: uuid
 # email: string
 # user_name: string
+# roles: [string]
 # password_digest: string
 # verify_code: integer
 # introduction: string
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920090923) do
+ActiveRecord::Schema.define(version: 20170922043902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170920090923) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest", default: "", null: false
+    t.string "role", default: [], null: false, array: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
