1595cfc391723fa40a9f78d6e521a0bc89315e0a
wangpeipei <wangpp_1995@163.com>
2017-09-27 16:01:18 +0800

    adjust webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
-const { environment } = require('./package');
+const { environment } = require('@rails/webpacker');
+
+// expose jquery
+environment.loaders.set('expose', {
+  test: require.resolve('jquery'),
+  use: [{
+    loader: 'expose-loader',
+    options: 'jQuery'
+  }, {
+    loader: 'expose-loader',
+    options: '$'
+  }]
+});
 
 module.exports = environment;
