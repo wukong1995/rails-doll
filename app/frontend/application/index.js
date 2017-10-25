1993d203762199ff0323d79eca457e808e50f777
wangpp <wangpp1995@gmail.com>
2017-10-25 17:16:58 +0800

    ADD modal

--- app/frontend/application/index.js
+++ app/frontend/application/index.js
@@ -1,12 +1,15 @@
 /* eslint no-console:0 */
 
 import '../style/app.scss';
-import deleteArticle from '../page/article';
-import particles from '../page/session';
+import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
-import { runPage } from '../util/tool';
+import article from './page/article';
+import particles from './page/session';
+
+import { runPage } from 'utils/tool';
 
 export const application = () => {
-  runPage('articles-index', deleteArticle);
+  runPage('articles-index', article);
   runPage(['sessions-index','sessions-new'], particles);
 };
