7ab6c106d669adccaf801389016a52c26e250a46
wangpeipei <wangpp_1995@163.com>
2017-10-30 18:07:34 +0800

    ADD react-rails

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,5 +1,11 @@
 import Rails from 'rails-ujs';
 import { application } from '../application';
 
+// Support component names relative to this directory:
+var componentRequireContext = require.context('components', true);
+var ReactRailsUJS = require('react_ujs');
+ReactRailsUJS.useContext(componentRequireContext);
+ReactRailsUJS.mountComponents();
+
 Rails.start();
 application();
