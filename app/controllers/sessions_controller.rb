a8d20aa6864201a2ed175b73b1128f7755c1870e
wangpeipei <wangpp_1995@163.com>
2017-10-31 19:54:21 +0800

    Adjust code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,26 +2,20 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: user_params[:email])
     if user != nil
-      flash[:error] = "用户名已存在， 请登录"
-      redirect_to signup_path
+      redirect_to signup_path, :alert => "用户名已存在， 请登录"
       return
     end
 
     if !verify_rucaptcha?(params[:_rucaptcha])
-      flash[:error] = "验证码不正确"
-      redirect_to signup_path
+      redirect_to signup_path, :alert => "验证码不正确"
       return
     end
 
-    user = User.new(user_params)
-    if user.save
-      redirect_to signup_path
-      return
-    else
-      flash[:error] = user.errors
-      redirect_to signup_path
-    end
+    @user = User.new(user_params)
 
+    if !@user.save
+      redirect_to signup_path, :alert => @user.errors
+    end
   end
 
   def index
@@ -51,7 +45,7 @@ class SessionsController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:email, :user_name, :password)
-  end
+    def user_params
+      params.require(:user).permit(:email, :user_name, :password)
+    end
 end
