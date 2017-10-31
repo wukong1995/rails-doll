dd2f2decedf094784861a08a2eb44ed40853a4ac
wangpeipei <wangpp_1995@163.com>
2017-10-31 17:21:22 +0800

    ADD article in admin

--- app/frontend/admin/index.js
+++ app/frontend/admin/index.js
@@ -1,7 +1,13 @@
 import './app.scss';
 
+import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import article from './page/article';
+import { runPage } from 'utils/tool';
+
 const admin = () => {
-  console.log('admin');
+  runPage('articles-index', article);
 };
 
 export default admin;
