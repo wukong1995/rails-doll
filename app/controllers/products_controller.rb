95ccf017d5697240a414ed022d8dbc065166d421
wangpeipei <wangpp_1995@163.com>
2017-11-24 16:32:37 +0800

    ADD show product in FE

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,5 @@
 class ProductsController < ApplicationController
-  def index
-    @articles = Article.order(created_at: :desc).page(params[:page]).per(10)
-  end
-
   def show
-    @article = Article.find(params[:id])
+    @product = Product.find(params[:id])
   end
 end
