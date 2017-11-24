bb9c64df8e95a922097ab9854280e43bd97e09ba
wangpp <wangpp1995@gmail.com>
2017-11-24 13:56:14 +0800

    ADD config for I18n

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module RailsApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.time_zone = 'Beijing'
+    config.i18n.enforce_available_locales = false
+    config.i18n.default_locale = 'zh-CN'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
