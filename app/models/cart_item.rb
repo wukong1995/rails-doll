8f5948d0c5401dafe28aeb401eb3ef8c2d7b1fbe
wangpp <wangpp1995@gmail.com>
2017-11-27 13:10:30 +0800

    Drop cart table

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,8 +1,6 @@
 class CartItem < ApplicationRecord
-  belong_to :cart
-  belong_to :product
-
-  acts_as_paranoid
+  belongs_to :user
+  belongs_to :product
 end
 
 # == Schema Information
@@ -10,18 +8,18 @@ end
 # Table name: cart_items
 #
 #  id         :uuid             not null, primary key
-#  cart_id    :uuid
+#  user_id    :uuid
 #  product_id :uuid
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 # Indexes
 #
-#  index_cart_items_on_cart_id     (cart_id)
 #  index_cart_items_on_product_id  (product_id)
+#  index_cart_items_on_user_id     (user_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (cart_id => carts.id)
 #  fk_rails_...  (product_id => products.id)
+#  fk_rails_...  (user_id => users.id)
 #
