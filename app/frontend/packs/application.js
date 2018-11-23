a7837452231362bd1da88801d8849006f41d45ca
Peipei Wong <wangpp1995@gmail.com>
2018-11-23 19:38:10 +0800

    fix cap deploy

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,11 +1,5 @@
 import Rails from 'rails-ujs';
 import application from '../application';
 
-// Support component names relative to this directory:
-var componentRequireContext = require.context('components', true);
-var ReactRailsUJS = require('react_ujs');
-ReactRailsUJS.useContext(componentRequireContext);
-ReactRailsUJS.mountComponents();
-
 Rails.start();
 application();
