600eb70097bbb0ee831d7d3de69a144ae428a2bc
Peipei <wangpp1995@gmail.com>
2018-12-02 19:03:23 +0800

    Remove rails-ujs after rails v5

--- app/frontend/packs/admin.js
+++ app/frontend/packs/admin.js
@@ -1,5 +1,3 @@
-import Rails from 'rails-ujs';
 import admin from '../admin';
 
-Rails.start();
 admin();
--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,5 +1,3 @@
-import Rails from 'rails-ujs';
 import application from '../application';
 
-Rails.start();
 application();
