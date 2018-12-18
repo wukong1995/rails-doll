ac3b8ac68ff2e11613a6ced09e681e68871e3927
Peipei Wong <wangpp1995@gmail.com>
2018-12-18 14:21:06 +0800

    Hidden product in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.order(created_at: :desc).page(params[:page]).per(12)
+    # @products = Product.order(created_at: :desc).page(params[:page]).per(12)
   end
 end
